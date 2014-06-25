28cb5ba51a8d870a1ae482a97b9da7b0a32ee908
Aaron Marriner <amarriner@amarriner.com>
2014-06-25 17:50:12 +0000

    Corrected corpus assembly

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Python script that assembles a haiku from a corpus of text. Determines syllables (in a fuzzy way) via the Wordnik API for hypenation. This is not perfect, but almost always gets it right.
 
 Currently using Jane Austen novels from Project Gutenberg.
+
+TODO: 
+ * Improve punctuation and capitalization in end result
--- haiku.py
+++ haiku.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
-from random import shuffle
-
 import json
 import keys
+import random
 import re
 import requests
 import twitter
 
+pwd = "/home/amarriner/python/haiku/"
+
 # Wordnik API URL
 wordnik_api_url = "http://api.wordnik.com:80/v4/word.json/<WORD>/hyphenation?useCanonical=false&limit=50&api_key=" + keys.wordnik_api_key
 
+# List of books to add to the corpus
 books = ['pride_and_prejudice.txt', 'emma.txt', 'sense_and_sensibility.txt']
 
 # Compiling regex patterns
@@ -21,15 +23,15 @@ s = re.compile(r"[ ]+")
 # Build a corpus of text
 corpus = ""
 for b in books:
-   f = open('corpus/' + b)
-   corpus = f.read().replace("\n", " ").replace("Mr.", "Mr").replace("Mrs.", "Mrs").replace("'s ", "s ")
+   f = open(pwd + 'corpus/' + b)
+   corpus = corpus + f.read().replace("\n", " ").replace("Mr.", "Mr").replace("Mrs.", "Mrs").replace("'s ", "s ") + ". "
    f.close()
 
 # Since Wordnik only allows a certain amount of API hits per hour, it's not feasible to run the entire
 # corpus through every time this script runs. Therefore, it will cache words its already looked up
 # in a syllables.json file to limit the amount of API calls. This next is just loading that file up
 # and pulling the existing data (if any) into a dict
-f = open('syllables.json')
+f = open(pwd + 'syllables.json')
 j = f.read()
 if len(j) > 0:
    syllables = json.loads(j)
@@ -42,16 +44,13 @@ corpus_list = []
 
 # Loop through the corpus via regex
 for sentence in re.findall(p, corpus):
-
-   # Since a sentence with more than 7 words would be too long for any haiku line, ignore those
-   #if len(sentence.split(" ")) < 7:
    c = sentence.strip().lower()
    c = m.sub(" ", c)
    c = s.sub(" ", c)
    corpus_list.append(c)
 
 haiku = []
-shuffle(corpus_list)
+random.shuffle(corpus_list)
 for sentence in corpus_list:
    total = 0
 
@@ -71,7 +70,9 @@ for sentence in corpus_list:
                syllables[w] = 1000
 
             # Write the current syllables dict out to disk for caching purposes (see above)
-            f = open('syllables.json', 'w')
+            # Inefficient to do at every word, but in case the script dies for some reason
+            # I want to limit API calls to Wordnik. Probably only an issue in testing, though 
+            f = open(pwd + 'syllables.json', 'w')
             f.write(json.dumps(syllables))
             f.close()
 
@@ -98,6 +99,6 @@ api = twitter.Api(keys.consumer_key, keys.consumer_secret, keys.access_token, ke
 status = api.PostUpdate(tweet + "\n\n#Haiku")
          
 # Write the current syllables dict out to disk for caching purposes (see above)
-f = open('syllables.json', 'w')
+f = open(pwd + 'syllables.json', 'w')
 f.write(json.dumps(syllables))
 f.close()
