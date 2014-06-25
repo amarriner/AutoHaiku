6e2e46faa888b5619747545faac219cc16ce06cd
Aaron Marriner <amarriner@amarriner.com>
2014-06-25 17:38:55 +0000

    Cleanup some comments and tweet structure

--- haiku.py
+++ haiku.py
@@ -6,6 +6,7 @@ import json
 import keys
 import re
 import requests
+import twitter
 
 # Wordnik API URL
 wordnik_api_url = "http://api.wordnik.com:80/v4/word.json/<WORD>/hyphenation?useCanonical=false&limit=50&api_key=" + keys.wordnik_api_key
@@ -52,17 +53,18 @@ for sentence in re.findall(p, corpus):
 haiku = []
 shuffle(corpus_list)
 for sentence in corpus_list:
-   # print sentence.strip()
    total = 0
 
    for w in sentence.split(" "):
       if len(w) > 0:
+
+         # If we don't know the number of syllables in this word, look it up on Wordnik
          if w not in syllables:
             lookups = lookups + 1
-            print "Looking up '" + w + "' on wordnik (" + str(lookups) + ") ..."
             request = requests.get(wordnik_api_url.replace("<WORD>", w))
             result = json.loads(request.content)
 
+            # Store the resulting number of syllables for later reference. Default to 1000 if unsure
             if len(result) > 0:
                syllables[w] = len(result)
             else:
@@ -73,21 +75,27 @@ for sentence in corpus_list:
             f.write(json.dumps(syllables))
             f.close()
 
+         # Keep track of the total number of syllables in this line so far and continue if it's over the limit
          total = total + syllables[w]
          if total > 7:
             continue
 
-   if len(haiku) == 0 and total == 5:
-      haiku.append(sentence)
-   if len(haiku) == 1 and total == 7:
-      haiku.append(sentence)
-   if len(haiku) == 2 and total == 5:
+   # Append line to the haiku list if it's the appropriate number of syllables
+   if (len(haiku) == 0 and total == 5) or (len(haiku) == 1 and total == 7) or (len(haiku) == 2 and total == 5):
       haiku.append(sentence)
    if len(haiku) == 3:
       break
 
+# Assemble the haiku list into a string with capitalization
+tweet = ""
 for line in haiku:
-   print line.capitalize().strip()
+   tweet = tweet + line.capitalize().strip() + "\n"
+
+# Connect to Twitter
+api = twitter.Api(keys.consumer_key, keys.consumer_secret, keys.access_token, keys.access_token_secret)
+
+# Tweet haiku
+status = api.PostUpdate(tweet + "\n\n#Haiku")
          
 # Write the current syllables dict out to disk for caching purposes (see above)
 f = open('syllables.json', 'w')
